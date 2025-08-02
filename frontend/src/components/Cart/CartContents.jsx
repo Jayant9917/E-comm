a536d4b14255e3c82a599ef4db2b215560d560ca
Jayant9917 <ranajayant527@gmail.com>
2025-08-03 02:38:21 +0530

    cartcomponent Added

---
+++ frontend/src/components/Cart/CartContents.jsx
@@ -0,0 +1,61 @@
+import { RiDeleteBin3Line } from 'react-icons/ri'
+
+const CartContents = () => {
+
+    const cartProducts = [
+        {
+            productId: 1,
+            name: 'T-shirt',
+            size: 'M',
+            color: 'Red',
+            quantity: 1,
+            price: 10,
+            image: 'https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',  
+        },
+        {
+            productId: 2,
+            name: 'Jeans',
+            size: 'M',
+            color: 'blue',
+            quantity: 1,
+            price: 10,
+            image: 'https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',  
+        }
+    ]
+
+  return (
+    <div>
+      {
+        cartProducts.map((product, index) => (
+          <div key={index} className="flex items-start justify-between py-4 border-b">
+            <div className='flex items-start'>
+                <img src={product.image} alt={product.name} className="w-20 h-24 object-cover mr-4 rounded"/> 
+                <div>
+                    <h3>{product.name}</h3>
+                    <p className='text-sm text-gray-500'>
+                        size: {product.size} | color: {product.color} 
+                    </p>
+                    <div className='flex items-center mt-2'>
+                        <button className='border rounded px-2 py-1 text-xl font-medium'> 
+                        -
+                        </button>
+                        <span className='mx-4'>{product.quantity}</span>
+                        <button className='border rounded px-2 py-1 text-xl font-medium'>
+                            +
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p>$ {product.price.toLocaleString()}</p>
+                <button>
+                    <RiDeleteBin3Line className='h-6 w-6 mt-2 text-red-600'/>
+                </button>
+            </div>
+          </div>
+        ))}
+    </div>
+  )
+}
+
+export default CartContents
--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -6,8 +6,15 @@ import {
 } from "react-icons/hi2";
 import SearchBar from "./SearchBar";
 import { useState } from "react";
+import CartDrawer from "../Layout/CartDrawer";
 
 const Navbar = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleCartDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
   const [searchOpen, setSearchOpen] = useState(false);
   return (
     <>
@@ -26,39 +33,53 @@ const Navbar = () => {
             </div>
             {/* Center - Navigation Links */}
             <div className="hidden md:flex space-x-6">
-              <Link to="#" className="text-gray-700 hover:text-black text-sm font-medium uppercase">
+              <Link
+                to="#"
+                className="text-gray-700 hover:text-black text-sm font-medium uppercase"
+              >
                 Men
               </Link>
-              <Link to="#" className="text-gray-700 hover:text-black text-sm font-medium uppercase">
+              <Link
+                to="#"
+                className="text-gray-700 hover:text-black text-sm font-medium uppercase"
+              >
                 Women
               </Link>
-              <Link to="#" className="text-gray-700 hover:text-black text-sm font-medium uppercase">
+              <Link
+                to="#"
+                className="text-gray-700 hover:text-black text-sm font-medium uppercase"
+              >
                 Top Wear
               </Link>
-              <Link to="#" className="text-gray-700 hover:text-black text-sm font-medium uppercase">
+              <Link
+                to="#"
+                className="text-gray-700 hover:text-black text-sm font-medium uppercase"
+              >
                 Bottom Wear
               </Link>
             </div>
             {/* Right - Social Icons */}
             <div className="flex items-center space-x-4">
               <Link to="/profile" className="hover:text-black">
-                <HiOutlineUser className="h-6 w-6 text-gray-700"/>
+                <HiOutlineUser className="h-6 w-6 text-gray-700" />
               </Link>
-              <button className="relative hover:text-black">
-                <HiOutlineShoppingBag className="h-6 w-6 text-gray-700"/>
+              <button onClick={toggleCartDrawer} 
+              className="relative hover:text-black">
+                <HiOutlineShoppingBag className="h-6 w-6 text-gray-700" />
                 <span className="absolute -top-1 bg-rabbit-red text-white text-xs rounded-full px-2 pu-0.5">
-                    4
+                  4
                 </span>
               </button>
               {/* Search */}
               <SearchBar isOpen={searchOpen} setIsOpen={setSearchOpen} />
               <button className="md:hidden">
-                <HiBars3BottomRight className="h-6 w-6 text-gray-700"/>
+                <HiBars3BottomRight className="h-6 w-6 text-gray-700" />
               </button>
             </div>
           </>
         )}
       </nav>
+      <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer}/>
     </>
   );
 };
