7ce7b530809321a63a9643037708bb73af74a551
Jayant9917 <ranajayant527@gmail.com>
2025-08-10 00:03:46 +0530

    admin userpage completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,48 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import UserLayout from './components/Layout/UserLayout.jsx'
-import Home from './pages/Home.jsx'
-import Login from './pages/Login.jsx'
-import Register from './pages/Resigter.jsx'
-import Profile from './pages/Profile.jsx'
-import { Toaster } from 'sonner'; 
-import CollectionPage from './pages/CollectionPage.jsx'
-import ProductDetails from './components/Products/ProductDetails.jsx'
-import Checkout from './components/Cart/Checkout.jsx'
-import OrderConfirmationPage from './pages/OrderConfirmationPage.jsx'
-import OrderDetailsPage from './pages/OrderDetailsPage.jsx'
-import MyOrdersPage from './pages/MyOrders.jsx'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserLayout from "./components/Layout/UserLayout.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Resigter.jsx";
+import Profile from "./pages/Profile.jsx";
+import { Toaster } from "sonner";
+import CollectionPage from "./pages/CollectionPage.jsx";
+import ProductDetails from "./components/Products/ProductDetails.jsx";
+import Checkout from "./components/Cart/Checkout.jsx";
+import OrderConfirmationPage from "./pages/OrderConfirmationPage.jsx";
+import OrderDetailsPage from "./pages/OrderDetailsPage.jsx";
+import MyOrdersPage from "./pages/MyOrders.jsx";
+import AdminLayout from "./components/Admin/AdminLayout.jsx";
+import AdminHomePage from "./pages/AdminHomePage.jsx";
+import UserManagement from "./components/Admin/UserManagement.jsx";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Toaster position="top-right" />
-    <Routes>
-      <Route path="/" element={<UserLayout/>}>
-      <Route index element={<Home/>}/>
-      <Route path="login" element={<Login/>} />
-      <Route path="register" element={<Register/>} />
-      <Route path="profile" element={<Profile/>} />
-      <Route path='collections/:collection' element={<CollectionPage/>}/>
-      <Route path='product/:id' element={<ProductDetails/>}/>
-      <Route path='checkout' element={<Checkout/>}/>
-      <Route path='order-confirmation' element={<OrderConfirmationPage/>}/>
-      <Route path='order/:id' element={<OrderDetailsPage/>}/>
-      <Route path='/my-orders' element={<MyOrdersPage/>}/>
-      </Route>
-      <Route>{/* Admin Layout */}</Route>
-    </Routes>
+      <Toaster position="top-right" />
+      <Routes>
+        <Route path="/" element={<UserLayout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="collections/:collection" element={<CollectionPage />} />
+          <Route path="product/:id" element={<ProductDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route
+            path="order-confirmation"
+            element={<OrderConfirmationPage />}
+          />
+          <Route path="order/:id" element={<OrderDetailsPage />} />
+          <Route path="/my-orders" element={<MyOrdersPage />} />
+        </Route>
+        {/* Admin Routes */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminHomePage />} />
+          <Route path="users" element={<UserManagement />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/Admin/AdminSidebar.jsx
@@ -0,0 +1,83 @@
+import { FaBoxOpen, FaClipboardList, FaSignOutAlt, FaStore, FaUser } from "react-icons/fa";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+
+const AdminSidebar = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="p-6">
+      <div className="mb-6">
+        <Link to="/" className="text-2xl font-medium">
+          Rabbit
+        </Link>
+      </div>
+      <h2 className="text-xl font-medium mb-6 text-center">Admin Dashboard</h2>
+
+      <nav className="flex flex-col space-y-2">
+        {/* users */}
+        <NavLink
+          to="/admin/users"
+          className={({ isActive }) =>
+            isActive
+              ? "bg-gray-700 text-white py-3 px-4 rounded flex items-center space-x-2"
+              : "text-gray-300 hover:bg-gray-700 hover:text-white py-3 px-4 rounded flex items-center space-x-2"
+          }
+        >
+          <FaUser />
+          <span>Users</span>
+        </NavLink>
+        {/* products */}
+        <NavLink
+          to="/admin/products"
+          className={({ isActive }) =>
+            isActive
+              ? "bg-gray-700 text-white py-3 px-4 rounded flex items-center space-x-2"
+              : "text-gray-300 hover:bg-gray-700 hover:text-white py-3 px-4 rounded flex items-center space-x-2"
+          }
+        >
+          <FaBoxOpen />
+          <span>Products</span>
+        </NavLink>
+        {/* orders */}
+        <NavLink
+          to="/admin/orders"
+          className={({ isActive }) =>
+            isActive
+              ? "bg-gray-700 text-white py-3 px-4 rounded flex items-center space-x-2"
+              : "text-gray-300 hover:bg-gray-700 hover:text-white py-3 px-4 rounded flex items-center space-x-2"
+          }
+        >
+          <FaClipboardList />
+          <span>Orders</span>
+        </NavLink>
+        {/* Shop */}
+        <NavLink
+          to="/admin/shop"
+          className={({ isActive }) =>
+            isActive
+              ? "bg-gray-700 text-white py-3 px-4 rounded flex items-center space-x-2"
+              : "text-gray-300 hover:bg-gray-700 hover:text-white py-3 px-4 rounded flex items-center space-x-2"
+          }
+        >
+          <FaStore />
+          <span className="">Shop</span>
+        </NavLink>
+      </nav>
+      {/* Button Logout */}
+      <div className="mt-6">
+        <button
+          className="w-full bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded flex items-center justify-center space-x-2"
+          onClick={handleLogout}
+        >
+            <FaSignOutAlt />
+            <span>Logout</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AdminSidebar;
