47046202ecd28fa577b198838024afb03d7755cb
Jayant9917 <ranajayant527@gmail.com>
2025-08-10 23:24:03 +0530

    product (C,U,D) Routes completed with data products.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,32 +4,36 @@ const User = require("../models/User");
 //Middleware to protect the routes
 
 const protect = async (req, res, next) => {
-    let token;
+  let token;
 
-    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
-        try {
-            token = req.headers.authorization.split(" ")[1];
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-            req.user = await User.findById(decoded.user.id).select("-password"); //Exclude password from response
-            next();
-        } catch (err) {
-            console.log("Token Verification Failed:", err);
-            return res.status(401).json({ message: "Token Verification Failed" });
-        }
-    } else {
-        return res.status(401).json({ message: "No Token Provided" });
+      req.user = await User.findById(decoded.user.id).select("-password"); //Exclude password from response
+      next();
+    } catch (err) {
+      console.log("Token Verification Failed:", err);
+      return res.status(401).json({ message: "Token Verification Failed" });
     }
+  } else {
+    return res.status(401).json({ message: "No Token Provided" });
+  }
 };
-
 
 //Middleware to check if the user is an admin
 const admin = (req, res, next) => {
-    if (req.user && req.user.role === "admin") {
-        next();
-    } else {
-        return res.status(403).json({ message: "You are not authorized as an admin" });
-    }
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    return res
+      .status(403)
+      .json({ message: "You are not authorized as an admin" });
+  }
 };
 
 module.exports = { protect, admin };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Product = require("../models/Product");
-const { protect, admin} = require("../middleware/authMiddleware");
+const { protect, admin } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
@@ -61,5 +61,97 @@ router.post("/", protect, admin, async (req, res) => {
     res.status(500).send("server error");
   }
 });
+
+// @route PUT /api/products/:id
+// @desc Update an exisiting product ID
+// @access Private
+router.put("/:id", protect, admin, async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      category,
+      brand,
+      sizes,
+      colors,
+      collections,
+      material,
+      gender,
+      image,
+      isFeatured,
+      isPublished,
+      tags,
+      dimensions,
+      weight,
+      sku,
+    } = req.body;
+
+    // Find the product by Id
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      //Update product fields
+      product.name = name || product.name;
+      product.description = description || product.description;
+      product.price = price || product.price;
+      product.discountPrice = discountPrice || product.discountPrice;
+      product.countInStock = countInStock || product.countInStock;
+      product.category = category || product.category;
+      product.brand = brand || product.brand;
+      product.sizes = sizes || product.sizes;
+      product.colors = colors || product.colors;
+      product.collections = collections || product.collections;
+      product.material = material || product.material;
+      product.gender = gender || product.gender;
+      product.image = image || product.image;
+      product.isFeatured =
+        isFeatured !== undefined ? isFeatured : product.isFeatured;
+      product.isPublished =
+        isPublished !== undefined ? isPublished : product.isPublished;
+      product.tags = tags || product.tags;
+      product.dimensions = dimensions || product.dimensions;
+      product.weight = weight || product.weight;
+      product.sku = sku || product.sku;
+
+      // Save the updated product
+      const updatedProduct = await product.save();
+      res.status(200).json(updatedProduct);
+    } else {
+      res.status(404).json({ message: "No product found with that ID" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send( "Server Error" );
+  }
+});
+
+// @route DELETE /api/products/:id
+// @desc Delete an existing product by ID
+// @access Private/Admin
+router.delete("/:id", protect, admin, async (req, res) => {
+  try{
+    // Find the product by Id
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      // Remove the product from DB
+      await product.deleteOne();
+      res.json({
+        message: "Product removed"
+      });
+    }else{
+      res.status(404).json({
+        message: "No product found with that ID"
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send( "Server Error" );
+  }
+});
+
 
 module.exports = router;
