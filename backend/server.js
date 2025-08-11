c337afcf0986d077fd449d5aae673361a288cdbb
Jayant9917 <ranajayant527@gmail.com>
2025-08-12 02:13:50 +0530

    Backend Complet

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,9 @@ const CheckoutRoutes = require('./routes/checkoutRoutes');
 const OrderRoutes = require('./routes/orderRoutes');
 const uploadRoutes = require('./routes/uploadRoutes');
 const SubscriberRoute = require('./routes/subscribeRoute');
+const adminRoutes = require('./routes/adminRoutes');
+const productAdminRoutes = require('./routes/productAdminRoutes');
+const adminOrderRoutes = require('./routes/adminOrderRoutes');
 
 const app = express();
 app.use(express.json());
@@ -34,6 +37,11 @@ app.use('/api/checkout', CheckoutRoutes);
 app.use('/api/orders', OrderRoutes);
 app.use('/api/upload', uploadRoutes);
 app.use('/api', SubscriberRoute);
+
+//Admin Routes
+app.use('/api/admin/users', adminRoutes);
+app.use('/api/admin/products', productAdminRoutes);
+app.use('/api/admin/orders', adminOrderRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
