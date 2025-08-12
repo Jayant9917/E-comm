95784975dbde9ca613faf0418b93e194694a523d
Jayant9917 <ranajayant527@gmail.com>
2025-08-12 05:40:41 +0530

    styling issue fixed

--- frontend/src/components/Layout/Hero.jsx
+++ frontend/src/components/Layout/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <img
         src={heroImg}
         alt="Rabbit Hero"
-        className="w-full h-[400px] md:h-[600px] lg:h-[750px] object-cover"
+        className="w-full h-[600px] md:h-[600px] lg:h-[750px] object-cover"
       />
       <div className="absolute inset-0 bg-black bg-opacity-5 flex items-center justify-center">
         <div className="text-center text-white p-6">
@@ -17,7 +17,7 @@ const Hero = () => {
           <p className="text-sm tracking-tighter md:text-lg mb-6">
             Explore our vacation-ready outfits with fast worldwide shopping.
           </p>
-          <Link to="#" className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg">
+          <Link to="#" className="bg-white text-gray-950 px-6 py-2 rounded-lg text-lg">
           Shop Now
           </Link>
         </div>
