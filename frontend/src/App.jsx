3e6c9eff05d7569522d66d80957699f56a1037e9
Jayant9917 <ranajayant527@gmail.com>
2025-08-16 10:05:37 +0530

    working on admin pannel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,38 +20,49 @@ import OrderManagement from "./components/Admin/OrderManagement.jsx";
 
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import ProtectedRoute from "./components/Common/ProtectedRoute.jsx";
 
 const App = () => {
   return (
     <Provider store={store}>
-    <BrowserRouter>
-      <Toaster position="top-right" />
-      <Routes>
-        <Route path="/" element={<UserLayout />}>
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="collections/:collection" element={<CollectionPage />} />
-          <Route path="product/:id" element={<ProductDetails />} />
-          <Route path="checkout" element={<Checkout />} />
+      <BrowserRouter>
+        <Toaster position="top-right" />
+        <Routes>
+          <Route path="/" element={<UserLayout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="profile" element={<Profile />} />
+            <Route
+              path="collections/:collection"
+              element={<CollectionPage />}
+            />
+            <Route path="product/:id" element={<ProductDetails />} />
+            <Route path="checkout" element={<Checkout />} />
+            <Route
+              path="order-confirmation"
+              element={<OrderConfirmationPage />}
+            />
+            <Route path="order/:id" element={<OrderDetailsPage />} />
+            <Route path="/my-orders" element={<MyOrdersPage />} />
+          </Route>
+          {/* Admin Routes */}
           <Route
-            path="order-confirmation"
-            element={<OrderConfirmationPage />}
-          />
-          <Route path="order/:id" element={<OrderDetailsPage />} />
-          <Route path="/my-orders" element={<MyOrdersPage />} />
-        </Route>
-        {/* Admin Routes */}
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<AdminHomePage />} />
-          <Route path="users" element={<UserManagement />} />
-          <Route path="products" element={<ProductManagement />} />
-          <Route path="products/:id/edit" element={<EditProductPage />} />
-          <Route path="orders" element={<OrderManagement />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+            path="/admin"
+            element={
+              <ProtectedRoute role="admin">
+                <AdminLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<AdminHomePage />} />
+            <Route path="users" element={<UserManagement />} />
+            <Route path="products" element={<ProductManagement />} />
+            <Route path="products/:id/edit" element={<EditProductPage />} />
+            <Route path="orders" element={<OrderManagement />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 };
--- frontend/src/pages/AdminHomePage.jsx
+++ frontend/src/pages/AdminHomePage.jsx
@@ -1,28 +1,39 @@
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchAllOrders } from "../redux/slices/adminOrderSlice";
+import { fetchAdminProducts } from "../redux/slices/adminProductSlice";
+
 
 const AdminHomePage = () => {
-  const orders = [
-    {
-      _id: 1234523,
-      user: {
-        name: "John Doe",
-      },
-      totalPrice: 100,
-      status: "Processing",
-    },
-  ];
+  const dispatch = useDispatch();
+  const { products, loading: productsLoading, error: productsError } = useSelector((state) => state.adminProducts);
+  const { orders, totalOrders, totalSales, loading: ordersLoading, error: ordersError } = useSelector((state) => state.adminOrders);
+
+  useEffect(() => {
+    dispatch(fetchAdminProducts());
+    dispatch(fetchAllOrders());
+  }, [dispatch]
+);
 
   return (
     <div className="max-w-7xl mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6">Admin Dashboard</h1>
+      {productsLoading || ordersLoading ? (
+        <div> Loading... </div>
+      ): productsError ? (
+        <p className="text-red-500 ">Error fetching products: {productsError}</p>
+      ): ordersError ? (
+        <p className="text-red-500 ">Error fetching orders: {ordersError}</p>
+      ): (
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         <div className="p-4 shadow-md rounded-lg">
           <h2 className="text-xl font-semibold">Revenue</h2>
-          <p className="text-2xl ">$1000</p>
+          <p className="text-2xl ">${totalSales.toFixed(2)}</p>
         </div>
         <div className="p-4 shadow-md rounded-lg">
           <h2 className="text-xl font-semibold">Total Orders</h2>
-          <p className="text-2xl ">200</p>
+          <p className="text-2xl ">${totalOrders}</p>
           <Link to="/admin/orders" className="text-blue-500 hover:underline">
             Manage Orders
           </Link>
@@ -35,6 +46,7 @@ const AdminHomePage = () => {
           </Link>
         </div>
       </div>
+      )}
       {/* Orders Table */}
       <div className="mt-6">
         <h2 className="text-2xl font-bold mb-4">Recent Orders</h2>
